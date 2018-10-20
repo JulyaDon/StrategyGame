d0919460e66bb88ef683bff34a1e808365e7a35d
JulyaDon <julya.p.don@gmail.com>
2018-10-20 15:20:34 +0300

    introduced model classes

---
+++ StrategyGame/Model/Heroes/Peasant.cs
@@ -0,0 +1,9 @@
+﻿namespace StrategyGame.Model.Heroes
+{
+    public class Peasant : IHero
+    {
+        public int Hitpoints { get; set; }
+        public int Damage { get; set; }
+        public int Speed { get; set; }
+    }
+}
---
+++ StrategyGame/Model/Heroes/Swordsman.cs
@@ -0,0 +1,9 @@
+﻿namespace StrategyGame.Model.Heroes
+{
+    public class Swordsman : IHero
+    {
+        public int Hitpoints { get; set; }
+        public int Damage { get; set; }
+        public int Speed { get; set; }
+    }
+}
